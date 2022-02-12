bca20b1f766158e448aae9e8c9120ee0164d3566
ct27 <gospodinkorolev@gmail.com>
2022-02-12 18:36:38 +0300

    EditIntarface

--- TheBellApp/wwwroot/js/main.js
+++ TheBellApp/wwwroot/js/main.js
@@ -1,4 +1,4 @@
-/*  ---------------------------------------------------
+﻿/*  ---------------------------------------------------
   Template Name: DJoz
   Description:  DJoz Music HTML Template
   Author: Colorlib
@@ -121,7 +121,7 @@
     /* var timerdate = "2020/01/01"; */
 
 	$("#countdown-time").countdown(timerdate, function(event) {
-        $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Days</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hours</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Seconds</p> </div>"));
+        $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Дней</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hours</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
     /*------------------
